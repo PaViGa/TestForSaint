ed28720c5099c111825b9b3f044f7121beb8ba6c
PaViGa <valiantigor@gmail.com>
2019-05-22 22:05:57 +0300

    Add project

---
+++ Saint - Junior Unity Developer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] craftMaterials;
+    [SerializeField]
+    private GameObject currentMaterial;
+    [SerializeField]
+    private float jumpForce;
+    private Vector3 localScaleCraftBlock;
+    private Rigidbody rb;
+    [SerializeField]
+    private int numberOfJumps;
+    [SerializeField]
+    private float durationJump;
+    private string currentTypeMaterial;
+	void Start () {
+        localScaleCraftBlock = new Vector3(1, 1, 1);
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        
+	}
+
+    private void MaterialActive(int i)
+    {
+        currentMaterial.SetActive(false);
+        currentMaterial = craftMaterials[i];
+        currentMaterial.SetActive(true);
+        
+    }
+
+    public void Jump()
+    {
+        rb.DOJump(transform.position, jumpForce, numberOfJumps, durationJump);
+    }
+
+    public void Remove()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+
+        if (Physics.Raycast(ray, out hit, 10))
+        {
+           if (hit.collider.tag == "Block")
+            {
+                Destroy(hit.collider.gameObject);
+            }
+
+        }
+    }
+
+    public void Craft()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+
+        if (Physics.Raycast(ray, out hit, 10))
+        {
+            if (hit.collider.tag == "Ground" && currentTypeMaterial == "Basement")
+            {
+                Vector3 hitPoint = hit.point;
+                hitPoint.x = Mathf.Round(hitPoint.x);
+                hitPoint.z = Mathf.Round(hitPoint.z);
+
+                GameObject obj = Instantiate(currentMaterial, hitPoint, Quaternion.identity);
+                obj.transform.localScale = localScaleCraftBlock;
+            }
+            else if(hit.collider.tag == "Block" && hit.normal.y == 1 && currentTypeMaterial == "Furniture")
+            {
+                Vector3 hitPoint = hit.collider.transform.position + hit.normal;
+                GameObject obj = Instantiate(currentMaterial, hitPoint, Quaternion.identity);
+                obj.transform.localScale = localScaleCraftBlock;
+            }
+
+        }
+    }
+
+    public void FirstSlot()
+    {
+        MaterialActive(0);
+        currentTypeMaterial = "Basement";
+    }
+
+    public void SecondSlot()
+    {
+        MaterialActive(1);
+        currentTypeMaterial = "Furniture";
+    }
+    
+}
---
+++ Saint - Junior Unity Developer/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+// Это заготовка кода под Character Controller, решил не соединять его с джойстиками,
+//а использовал готовый код из асета.
+
+
+public class PlayerCtrl : MonoBehaviour {
+
+float speed, jumpForce, gravity, sensitivity;
+    GameObject player;
+    CharacterController controller;
+    Vector3 moveDirection, cameraRotation;
+    Transform cameraTransform;
+    GameObject[] craftMaterials;
+        private void Awake()
+        {
+            
+            speed = 7.5f;
+            jumpForce = 1;
+            gravity = 2;
+            sensitivity = 50;
+            if (Camera.main != null)
+                cameraTransform = Camera.main.transform;
+            Cursor.lockState = CursorLockMode.Locked;
+            controller = GameObject.Find("Player").GetComponent<CharacterController>();
+            player = GameObject.FindWithTag("Player");
+        }
+
+    private void Update()
+    {
+        MouseRotation();
+        InputHandler();
+    }
+
+    public void MouseRotation()
+        {
+            float sens = sensitivity * Time.deltaTime;
+            float mouseX = Input.GetAxis("Mouse X") * sens;
+            player.transform.Rotate(Vector3.up * mouseX);
+            float mouseY = Input.GetAxis("Mouse Y") * sens;
+            cameraRotation.x -= mouseY;
+            cameraRotation.x = Mathf.Clamp(cameraRotation.x, -65, 65);
+            cameraTransform.localEulerAngles = cameraRotation;
+        }
+
+        public void InputHandler()
+        {
+            if (controller.isGrounded)
+            {
+                float h = Input.GetAxis("Horizontal");
+                float v = Input.GetAxis("Vertical");
+                moveDirection = new Vector3(h, 0, v);
+                moveDirection = player.transform.TransformDirection(moveDirection);
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    moveDirection.y = jumpForce;
+                }
+            }
+
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * speed * Time.deltaTime);
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            
+        }
+}
